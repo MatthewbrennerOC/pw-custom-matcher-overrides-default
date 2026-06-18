5edde385a02abb701a3a3a090e9e294d01793e2d
Matthew Brenner <matthew.brenner@omnicell.com>
2026-06-18 13:36:05 -0600

    reproduce bug

---
+++ tests/example.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+export const aCustomExpect = expect.extend({
+  async toBeVisible(pageObjectWouldGoHere: any) {
+    const assertionName = 'toBeVisible';
+    const customSearchLocator = pageObjectWouldGoHere;
+
+    let pass: boolean;
+    let matcherResult: any;
+    try {
+      const expectation = this.isNot ? expect(customSearchLocator).not : expect(customSearchLocator);
+      await expectation.toBeVisible();
+      pass = true;
+    } catch (e: any) {
+      matcherResult = e.matcherResult;
+      pass = false;
+    }
+
+    if (this.isNot) {
+      pass = !pass;
+    }
+
+    const message = pass
+        ? () =>
+            this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
+            '\n\n' +
+            `Locator: ${customSearchLocator}\n` +
+            `Expected: not visible\n` +
+            (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '')
+        : () =>
+            this.utils.matcherHint(assertionName, undefined, undefined, { isNot: this.isNot }) +
+            '\n\n' +
+            `Locator: ${customSearchLocator}\n` +
+            `Expected: visible\n` +
+            (matcherResult ? `Received: ${this.utils.printReceived(matcherResult.actual)}` : '');
+
+    return {
+      message,
+      pass,
+      name: assertionName,
+      expected: 'visible',
+      actual: matcherResult?.actual,
+    };
+  },
+});
